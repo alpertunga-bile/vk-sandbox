bebd48d87c55aa491b56a8a3d340a391d7d3e551
alpertunga-bile <kingdom19online@hotmail.com>
2023-06-27 00:56:25 +0300

    Vulkan initialization functions are added

--- includes/vk_engine.hpp
+++ includes/vk_engine.hpp
@@ -1,16 +1,31 @@
 #pragma once
 #include "vk_types.hpp"
+#include <vector>
 
 class VulkanEngine
 {
-    public:
+    private:
         VkExtent2D _windowExtent{1280, 720};
         struct SDL_Window* _window{nullptr};
 
+        VkInstance _instance;
+        VkDebugUtilsMessengerEXT _debugMessenger;
+        VkPhysicalDevice _physicalDevice;
+        VkDevice _device;
+        VkSurfaceKHR _surface;
+
+        VkSwapchainKHR _swapchain;
+        VkFormat _swapchainImageFormat;
+        std::vector<VkImage> _swapchainImages; // actual image
+        std::vector<VkImageView> _swapchainImageViews; // wrapper for image
 
     public:
         void init();
         void cleanup();
         void draw();
         void run();
+
+    private:
+        void initVulkan();
+        void initSwapchain();
 };
--- sources/vk_engine.cpp
+++ sources/vk_engine.cpp
@@ -2,8 +2,86 @@
 #include "../includes/vk_types.hpp"
 #include "../includes/vk_initializers.hpp"
 
+#include "../includes/VkBootstrap.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
+#include "vk_engine.hpp"
+
+/*
+Define check macro
+*/
+
+#define VK_CHECK(res) vk_check(res, __FILE__, __LINE__)
+
+static void vk_check(VkResult result, const char* file, int line)
+{
+	if (result == VK_SUCCESS)
+    {
+        return;
+    }
+
+	printf("::VK_CHECK:: [%s] %d\n", file, line);
+    exit(1);
+}
+
+/*
+Define VulkanEngine functions
+*/
+
+void VulkanEngine::initVulkan()
+{
+    // ---------------------------------------------------------------------
+    // Instance
+    // ---------------------------------------------------------------------
+
+    vkb::InstanceBuilder instanceBuilder;
+
+    auto instanceRef = instanceBuilder.set_app_name("Vulkan Sandbox")
+                        .request_validation_layers(true)
+                        .require_api_version(1, 1, 0)
+                        .use_default_debug_messenger()
+                        .build();
+    
+    vkb::Instance vkbInstance = instanceRef.value();
+
+    _instance = vkbInstance.instance;
+    _debugMessenger = vkbInstance.debug_messenger;
+
+    // ---------------------------------------------------------------------
+    // Physical Device & Device
+    // ---------------------------------------------------------------------
+    // create actual window to render
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+    vkb::PhysicalDeviceSelector deviceSelector{vkbInstance};
+    vkb::PhysicalDevice physicalDevice = deviceSelector.set_minimum_version(1, 1)
+                                        .set_surface(_surface)
+                                        .select()
+                                        .value();
+
+    vkb::DeviceBuilder deviceBuilder{physicalDevice};
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    _device = vkbDevice.device;
+    _physicalDevice = physicalDevice.physical_device;                                    
+}
+
+void VulkanEngine::initSwapchain()
+{
+    vkb::SwapchainBuilder swapchainBuilder{_physicalDevice, _device, _surface};
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+                                .use_default_format_selection()
+                                // strong vsync
+                                .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+                                .set_desired_extent(_windowExtent.width, _windowExtent.height)
+                                .build()
+                                .value();
+
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+    _swapchainImageFormat = vkbSwapchain.image_format; 
+}
 
 void VulkanEngine::init()
 {
@@ -12,13 +90,16 @@ void VulkanEngine::init()
 
     // create window
     _window = SDL_CreateWindow(
-        "PXDVulkan",
+        "Vulkan Sandbox",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         _windowExtent.width,
         _windowExtent.height,
         windowFlags
     );
+
+    initVulkan();
+    initSwapchain();
 }
 
 void VulkanEngine::draw()
@@ -48,6 +129,19 @@ void VulkanEngine::run()
 
 void VulkanEngine::cleanup()
 {
+    // images are destroyed with swapchain
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+    for(int i = 0; i < _swapchainImageViews.size(); i++)
+    {
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+    }
+
+    vkDestroyDevice(_device, nullptr);
+    vkDestroySurfaceKHR(_instance, _surface, nullptr);
+    vkb::destroy_debug_utils_messenger(_instance, _debugMessenger);
+    vkDestroyInstance(_instance, nullptr);
+
     if(_window != nullptr)
     {
         SDL_DestroyWindow(_window);
